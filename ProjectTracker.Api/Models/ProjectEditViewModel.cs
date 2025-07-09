a85a59cd6b3bf95aaea2f56bfb0784948ae7a589
Jinas Muhammed <ee000jinas@gmail.com>
2025-07-09 22:53:18 +0400

    Add ProjectEditViewModel and related updates

---
+++ ProjectTracker.Api/Models/ProjectEditViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectTracker.Api.Models
+{
+  
+    public class ProjectEditViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+    }
+}
